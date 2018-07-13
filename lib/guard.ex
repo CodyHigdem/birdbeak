7732b8e713128d2b5295220391000951c32bbe99
Cody <meanderingmonarch@gmail.com>
2018-07-13 14:52:42 -0500

    BUG: more typos

--- lib/guard.ex
+++ lib/guard.ex
@@ -2,18 +2,18 @@ defmodule Birdbeak.Guardian do
   use Guardian, otp_app: :birdbeak
 
   def subject_for_token(user, _claims) do
-    sub = to_string(user_id)
+    sub = to_string(user.id)
     {:ok, sub}
   end
 
-  def subject_for_token(_,_) do
+  def subject_for_token(_, _) do
     {:error, :reason_for_error}
   end
 
   def resource_from_claims(claims) do
     id = claims["sub"]
-    resource = Birdbeak.Accounts.get_user!(id)
-    {:ok, resource}
+    resource = MyApi.Accounts.get_user!(id)
+    {:ok,  resource}
   end
 
   def resource_from_claims(_claims) do
