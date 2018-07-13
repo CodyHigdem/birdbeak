c615e4cdafd25864a49a9f03b2ee8e9870e51442
Cody <meanderingmonarch@gmail.com>
2018-07-13 16:05:08 -0500

    feature: signin-jwt, building the user_controller needed

--- lib/birdbeak/accounts/accounts.ex
+++ lib/birdbeak/accounts/accounts.ex
@@ -15,6 +15,14 @@ defmodule Birdbeak.Accounts do
 
   alias Birdbeak.Accounts.User
 
+  def token_sign_in(email, password) do
+    case email_password_auth(email, password) do
+      {:ok, user} ->
+        Guardian.encode_and_sign(user)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
   defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
     with {:ok, user} <- get_by_email(email),
     do: verify_password(password, user)
