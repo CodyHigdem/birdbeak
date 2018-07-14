92f51575fcc3e6f9e7f6242be0373943fe17e9ef
Cody <meanderingmonarch@gmail.com>
2018-07-13 19:44:41 -0500

    BUG: fixed missing end on function

--- lib/birdbeak/accounts/accounts.ex
+++ lib/birdbeak/accounts/accounts.ex
@@ -23,6 +23,7 @@ defmodule Birdbeak.Accounts do
         {:error, :unauthorized}
     end
   end
+
   defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
     with {:ok, user} <- get_by_email(email),
     do: verify_password(password, user)
@@ -32,19 +33,19 @@ defmodule Birdbeak.Accounts do
     case Repo.get_by(User, email: email) do
       nil ->
         dummy_checkpw()
-        {:error, "Login Error"}
+        {:error, "Login error."}
       user ->
         {:ok, user}
     end
   end
 
   defp verify_password(password, %User{} = user) when is_binary(password) do
-    if checkpw(password, user.password_hash) do
-      {:ok, user}
-    else
-      {:error, :invalid_password}
-    end
-  end
+     if checkpw(password, user.password_hash) do
+       {:ok, user}
+     else
+       {:error, :invalid_password}
+     end
+   end
 
   @doc """
   Returns the list of users.
